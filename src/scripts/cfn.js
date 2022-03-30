632a8b7cf80179cda735f3d6afe6630e8c08bfb9
Nathan <nathanrv98@gmail.com>
2022-03-30 18:23:12 -0400

    tooltip and interactions working

--- src/scripts/cfn.js
+++ src/scripts/cfn.js
@@ -4,33 +4,36 @@
 */
 import d3Tip from 'd3-tip'
 
+let radius = 10
+
 export function load(){
     let margin = {top: 10, right: 30, bottom: 30, left: 250};
     let width = 1200 - margin.left - margin.right;
-    let height = 800 - margin.top - margin.bottom;
+    let height = 500 - margin.top - margin.bottom;
 
     let svg = d3.select("#cfn-chart")
         .append("svg")
+            .attr("id","cfn-svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
         .append("g")
+            .attr("id","cfn-g")
             .attr("transform", "translate(" + margin.left + "," + margin.top +")");   
 
 
     let minTip = d3Tip().attr("class","d3-tip")
-        .offset([-10,20])
+        .direction("w")
         .html(function(d){
             return getMinContents(d);
         });
 
     let meanTip = d3Tip().attr("class","d3-tip")
-        .offset([90,20])
         .html(function(d){
             return getMeanContents(d);
         })
     
     let maxTip = d3Tip().attr("class","d3-tip")
-        .offset([0,20])
+        .direction("e")
         .html(function(d){
             return getMaxContents(d);
         })
@@ -43,12 +46,15 @@ export function load(){
         data = transformValues(data);
 
         xScale = createXAxis(svg, data, width, height);
-        yScale = createYAxis(svg, data, height);
+        yScale = createYScale(data, height);
         
         data = regroupBySituation(data);
+        console.log(data)
+        groups = createGroups(data, tips, yScale)
+        createYAxis(groups, yScale);
 
-        lines = createLines(svg, tips, data, xScale, yScale);
-        createCircles(svg, tips, data, xScale, yScale);
+        lines = createLines(groups, xScale);
+        createCircles(groups, xScale);
     })
 }
 
@@ -91,8 +97,8 @@ function regroupBySituation(data){
         let group = groupedData[situation]
 
         let tmp = group[0]
-        tmp["Valeur_" + tmp["Pays"]] = tmp["Valeur"]
-        tmp["Valeur_" + group[1]["Pays"]] = group[1]["Valeur"]
+        tmp[tmp["Pays"]] = tmp["Valeur"]
+        tmp[group[1]["Pays"]] = group[1]["Valeur"]
 
         delete tmp['Valeur']
         delete tmp['Pays']
@@ -102,6 +108,37 @@ function regroupBySituation(data){
 
     return formattedData;
 }
+
+/**
+ * Creates the groups for the grouped bar chart and appends them to the graph.
+ * Each group corresponds to an act.
+ *
+ * @param {object[]} data The data to be used
+ * @param {*} y The graph's y scale
+ */
+ function createGroups (data, tips, y) {
+    return d3.select("#cfn-g").selectAll('g.situation_familiale')
+        .data(data)
+        .enter()
+        .append('g')
+        .attr('transform',function(d){return 'translate(0,'+ (y.bandwidth()/2 + y(d.Situation_familiale)) +')';})
+        .attr('class','situation_familiale')
+        .attr("id",function(d){return d.Situation_familiale})
+        .on("mouseover", function(d){showTips(tips, this, d, xScale); selectGroup(this.id);})
+        .on("mouseout", function(d){hideTips(tips, this, d); unselectGroup();})  
+  }
+
+  function unselectGroup(group){
+    d3.selectAll('g.situation_familiale')
+        .style('fill', "black")
+  }
+
+  function selectGroup(group){
+    d3.selectAll('g.situation_familiale')
+        .style('fill', function(d){
+            if(d.Situation_familiale == group) return "black";
+            return 'grey';})
+  }
 
 function createXAxis(svg, data, width, height){
     let minValue = d3.min(data, (d)=>{
@@ -116,9 +153,14 @@ function createXAxis(svg, data, width, height){
         .domain([minValue, maxValue])
         .range([0, width])
 
-    let xAxis = svg.append("g")
+    let xAxis = d3.axisBottom(xScale)
+        .tickFormat(function(d){
+            return d+'%';
+        })
+    
+    svg.append("g")
         .attr("transform","translate(0," + height + ")")
-        .call(d3.axisBottom(xScale))
+        .call(xAxis)
         .style("font-size", "15px")
         .style("font-weight", "bold")
     
@@ -126,97 +168,64 @@ function createXAxis(svg, data, width, height){
     return xScale;
 }
 
-
-function createYAxis(svg, data, height){
-
+function createYScale(data, height){
     let yScale = d3.scaleBand()
         .domain(data.map(function(d){
             return d.Situation_familiale;
         }))
         .range([0, height])
 
-    let yAxis = svg.append("g")
-        .attr("transform", "translate(0,0)")
-        .call(d3.axisLeft(yScale))
+    return yScale;
+}
+
+function createYAxis(groups, yScale){
+    groups.append("text")
+        .attr("transform", "translate(-240,0)")
+        .text(function(d){return d.Situation_familiale;})
         .style("font-size", "15px")
         .style("font-weight", "bold")
-    
 
     return yScale;
 }
 
-function createLines(svg, tips, data, xScale, yScale){
-    svg.selectAll(".cfn-line")
-        .data(data)
-        .enter()
-        .append("line")
+function createLines(groups, xScale){
+    groups.append("line")
             .attr("x1", function(d){
                 return xScale(getMin(d))
             })
             .attr("x2", function(d){
                 return xScale(getMax(d))
             })
-            .attr("y1", function(d){ return yScale(d.Situation_familiale) + yScale.bandwidth()/2})
-            .attr("y2", function(d){ return yScale(d.Situation_familiale) + yScale.bandwidth()/2})
+            .attr("y1", 0)
+            .attr("y2", 0)
             .attr("stroke","grey")
             .attr("stroke-width","5px")
-            .on("mouseover", function(d){showTipsLine(tips, this, d, xScale)})
-            .on("mouseout", function(d){hideTips(tips, this, d)})  
 }
 
-function createCircles(svg, tips, data, xScale, yScale){
-    radius = 10
+function createCircles(groups, xScale){
 
-    svg.selectAll(".cfn-circle")
-    .data(data)
-    .enter()
-    .append("circle")
+    groups.append("circle")
         .attr("cx", function(d){
             return xScale(d["Moyenne OCDE"])
         })
-        .attr("cy", function(d){ return yScale(d.Situation_familiale) + yScale.bandwidth()/2})
+        .attr("cy", 0)
         .attr("r", radius)
-        .attr("fill", "grey")
-        .on("mouseover", function(d){showTipsMean(tips, this, d, xScale)})
-        .on("mouseout", function(d){hideTips(tips, this, d)})  
+        .attr("opacity", 0.7)
+        .attr("fill", "#95C71B")
 
-    svg.selectAll(".cfn-circle")
-        .data(data)
-        .enter()
-        .append("circle")
-            .attr("cx", function(d){
-                return xScale(d3.min([d.Valeur_Quebec, d.Valeur_Suede]))
-            })
-            .attr("cy", function(d){ return yScale(d.Situation_familiale) + yScale.bandwidth()/2})
+    groups.append("circle")
+            .attr("cx", function(d){return xScale(d.Quebec)})
+            .attr("cy", 0)
             .attr("r",radius)
-            .attr("fill",function(d){
-                // TODO implement colorscale
-                if (d3.min([d.Valeur_Quebec, d.Valeur_Suede]) == d.Valeur_Quebec){
-                    return "blue";
-                }
-                return "yellow";
-            })
-            .on("mouseover", function(d){showTipsMin(tips, this, d, xScale)})
-            .on("mouseout", function(d){hideTips(tips, this, d)})  
+            .attr("opacity", 0.7)
+            .attr("fill","#001F97")
 
-    svg.selectAll(".cfn-circle")
-        .data(data)
-        .enter()
-        .append("circle")
-            .attr("cx", function(d){
-                return xScale(d3.max([d.Valeur_Quebec, d.Valeur_Suede]))
-            })
-            .attr("cy", function(d){ return yScale(d.Situation_familiale) + yScale.bandwidth()/2})
-            .attr("r", radius)
-            .attr("fill",function(d){
-                // TODO implement colorscale
-                if (d3.max([d.Valeur_Quebec, d.Valeur_Suede]) == d.Valeur_Quebec){
-                    return "blue";
-                }
-                return "yellow";
-            })         
-            .on("mouseover", function(d){showTipsMax(tips, this, d, xScale)})
-            .on("mouseout", function(d){hideTips(tips, this, d)})  
+    groups.append("circle")
+            .attr("cx", function(d){return xScale(d.Suede)})
+            .attr("cy", 0)
+            .attr("r",radius)
+            .attr("opacity", 0.7)
+            .attr("fill","#FFCD00")       
 }
 
 function getMinContents(data){
@@ -235,17 +244,17 @@ function getMaxContents(data){
 }
 
 function getMin(data){
-    return d3.min([data.Valeur_Quebec,data.Valeur_Suede,data["Moyenne OCDE"]]);
+    return d3.min([data.Quebec,data.Suede,data["Moyenne OCDE"]]);
 }
 
 function getMax(data){
-    return d3.max([data.Valeur_Quebec,data.Valeur_Suede,data["Moyenne OCDE"]]);
+    return d3.max([data.Quebec,data.Suede,data["Moyenne OCDE"]]);
 }
 
 function getMiddle(data){
     let minValue = getMin(data);
     let maxValue = getMax(data);
-    let value = [data.Valeur_Quebec,data.Valeur_Suede,data["Moyenne OCDE"]]
+    let value = [data.Quebec,data.Suede,data["Moyenne OCDE"]]
         .filter((value) => {return (value != minValue && value != maxValue)})[0];
     
     return value;
@@ -264,28 +273,27 @@ function getContentString(value){
 
 function getState(data, value){
     switch(value){
-        case data.Valeur_Quebec:
+        case data.Quebec:
             return "Québec"
-        case data.Valeur_Suede:
+        case data.Suede:
             return "Suède"
         case data["Moyenne OCDE"]:
             return "Moyenne OCDE"
     }
 }
 
-function showTipsLine(tips, object, data, xScale){
+function showTips(tips, object, data, xScale){
     //Min tip
-    tips[0].offset([10,
-        -object.getTotalLength()/2 - 40
-        ])
-    //Mean tip
-    tips[1].offset([-10,
-        (xScale(getMiddle(data)) - xScale(getMin(data))) - object.getTotalLength()/2
-        ])
+    let lineLength = xScale(getMax(data)) - xScale(getMin(data))
+    tips[0].offset([0,object.getBBox().width - lineLength - 3*radius])
+
+    //Middle tip
+    let distance_max_middle = xScale(getMax(data)) - xScale(getMiddle(data))
+    let offset = object.getBBox().width/2 - distance_max_middle
+    tips[1].offset([-radius, offset-radius])
+
     //Max tip
-    tips[2].offset([10,
-        object.getTotalLength()/2 + 40
-        ])
+    tips[2].offset([0,radius])
 
     tips.forEach((tip)=>{
         tip.show(data, object)
@@ -351,6 +359,7 @@ function showTipsMax(tips, object, data, xScale){
 
 function hideTips(tips, object, data){
     tips.forEach((tip)=>{
+        tip.offset([0,0])
         tip.hide(data, object)
     })
 }
