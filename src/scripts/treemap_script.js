6ffd5d9c65a8c1563ff23d682e740c2dba5c80fa
Benjamin Boucher-Charest <47901054+BenjaminB-Charest@users.noreply.github.com>
2022-04-07 21:15:45 -0400

    text works properly in treemap

--- src/scripts/treemap_script.js
+++ src/scripts/treemap_script.js
@@ -4,8 +4,8 @@ export function load(){
   
 // set the dimensions and margins of the graph
 const margin = {top: 10, right: 10, bottom: 10, left: 10},
-  width = 445 - margin.left - margin.right,
-  height = 445 - margin.top - margin.bottom;
+  width = 500 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
 
 // append the svg object to the body of the page
@@ -37,7 +37,7 @@ var svg = d3.select("#treemap")
       "Impôts sur le patrimoine","Impôts sur la consommation",
       "Autres impôts"
       ])
-      .range([ "#402D54", "#D18975", "#8FD175", "#FFFFFF", "#8FD175","#8FD175", "#8FD175"])
+      .range(["#F8B195","#F67280","#C06C84","#6C5B7B","#355C7D","#99B898", "#2A363B"])
     // use this information to add rectangles:
  svg
     .selectAll("rect")
@@ -46,10 +46,10 @@ var svg = d3.select("#treemap")
     .append("rect")
       .attr('x', function (d) { return d.x0; })
       .attr('y', function (d) { return d.y0; })
-      .attr('width', function (d) { return d.x1 - d.x0; })
-      .attr('height', function (d) { return d.y1 - d.y0; })
+      .attr('width', function (d) { return (d.x1 - d.x0 + 100); })
+      .attr('height', function (d) { return (d.y1 - d.y0 + 100); })
       .style("stroke", "black")
-      .style("fill", "slateblue")
+      .style("fill", function (d) { return color(d.id)})
 
 
     // and to add the text labels
@@ -60,9 +60,22 @@ var svg = d3.select("#treemap")
       .append("text")
         .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
         .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
-        .text(function(d){ return d.data.Indicator})
-        .attr("font-size", "19px")
-        .attr("fill", "white")
+        .text(function(d){ return d.id})
+        .attr("font-size", "14px")
+      .attr("fill", "white")
+    
+    svg
+    .selectAll("vals")
+    .data(root.leaves())
+    .enter()
+    .append("text")
+      .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
+      .attr("y", function(d){ return d.y0+35})    // +20 to adjust position (lower)
+       .text(function (d) {
+         return Math.round(d.value * 100) + '%'
+       })
+      .attr("font-size", "16px")
+      .attr("fill", "white")
   });
 
 
