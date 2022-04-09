fe91003ba168a83f1b2d57b1259c631f558b8568
just13564 <justin.caisse@gmail.com>
2022-04-09 19:06:41 -0400

    j'ai mes div de map

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -3,22 +3,20 @@ import { geoMercator } from "d3-geo"
 
 export function load() {
 
-    drawMap(require('./quebec.json'), "#001F97", [-40.708879, 55.579611], "quebec")
-    drawMap(require('./sweden.json'), "#FFCD00", [15.368631, 62.346091], "sweden")
+    drawMap(require('./quebec.json'), "#001F97", [-40.708879, 51.75], "quebec", 285, 380)
+    drawMap(require('./sweden.json'), "#FFCD00", [50, 60.5], "sweden", 170, 372)
 
 
-    function drawMap(data, color, coordinate, place) {
-        d3.select('#map')
+    function drawMap(data, color, coordinate, place, width, height) {
+        d3.select("#" + place + "-map")
             .select("." + place + "-map-svg")
-            .attr('width', 800)
-            .attr('height', 625)
+            .attr('width', width)
+            .attr('height', height).style("background", "black")
 
-        d3.select('.graph')
+        d3.select("." + place + "-graph")
             .select('svg')
             .append('g')
             .attr('id', place + '-map-g')
-            .attr('width', 800)
-            .attr('height', 625)
 
         var projection = geoMercator()
             .center(coordinate)
