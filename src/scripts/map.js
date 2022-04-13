36183a22e16aa7543e59f8a54fa4a4f945af64bb
Justin <Justin@DESKTOP-1UFLGCM>
2022-04-13 01:26:11 -0400

    scrolly viz 1 finish

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,5 +1,6 @@
 import { geoPath } from "d3-geo"
 import { geoMercator } from "d3-geo"
+import line from "d3-geo/src/clip/line"
 
 const coordLineQuebec1 = {
     x1: 100,
@@ -144,8 +145,7 @@ export function load() {
     var svgQuebec = createCountrySvg("quebec", dimensionQuebecInit)
     var svgSweden = createCountrySvg("sweden", dimensionSwedenInit)
 
-    d3.select(".quebec-graph").style("margin-left", String(-dimensionQuebecInit.width / 2) + "px")
-    d3.select(".sweden-graph").style("margin-right", String(-dimensionSwedenInit.width / 2) + "px")
+    d3.select(".quebec-graph").style("left", String(window.innerWidth / 2 - dimensionQuebecInit.width / 2) + "px")
 
     drawPictogram(svgQuebec, populationQuebec, coordPictoQuebec, "quebec", coordLineQuebecPicto1, coordLineQuebecPicto2, "8,485 millions habitants", textCoordPictoQuebec)
     drawMap(svgQuebec, "quebec", "1 542 056 km2", textCoordQuebec, [-49.708879, 53.75], require('./quebec.json'), "#001F97", coordLineQuebec1, coordLineQuebec2, dimensionQuebecInit.width)
@@ -160,29 +160,85 @@ export function load() {
 
     window.addEventListener('scroll', function(e) {
 
-        console.log(this.document.documentElement.scrollTop)
+        const windowWidth = this.window.innerWidth
         const posScroll = this.document.documentElement.scrollTop
 
         if (posScroll > 0 && posScroll < 875) {
 
+            d3.selectAll(".picto-country").style("display", "none")
+            d3.selectAll(".legend-picto").style("display", "none")
+
+            const rightSweden = (windowWidth - dimensionSwedenInit.width) / 2 - (dimensionQuebecInit.width / 2 + 45) * posScroll / 875
+            const leftQuebec = (windowWidth - dimensionQuebecInit.width) / 2 - (dimensionSwedenInit.width / 2 + 45) * posScroll / 875
+
             d3.select(".sweden-graph")
                 .style("display", "block")
                 .style("position", "fixed")
-                .style("right", String(50 - 15 * posScroll / 875) + "%")
+                .style("right", String(rightSweden) + "px")
                 .style("top", String(950 - posScroll) + "px")
 
-            d3.select(".quebec-graph")
-                .style("left", String(50 - 15 * posScroll / 875) + "%")
+            d3.select(".quebec-graph").style("left", String(leftQuebec) + "px")
 
-        } else if (posScroll >= 875) {
+            replaceElementScrolly(0, coordLineSwedenInit1.x1, coordLineSwedenInit1.x2, coordLineSwedenInit2.x1, coordLineSwedenInit2.x2, textCoordSwedenInit.x, dimensionQuebecInit.width, dimensionSwedenInit.width)
+
+        } else if (posScroll >= 875 && posScroll < 1200) {
+
+            statePicto("none")
+
+            const sideCountryInit = (windowWidth - dimensionSwedenInit.width - dimensionQuebecInit.width) / 2 - 45
+            const sideCountryFactor = (dimensionQuebecFinal.width + dimensionSwedenFinal.width - dimensionQuebecInit.width - dimensionSwedenInit.width) / 2
+            const factor = (posScroll - 875) / (1200 - 875)
 
             d3.select(".sweden-graph")
-                .style("right", "35%")
+                .style("right", String(sideCountryInit - sideCountryFactor * factor) + "px")
                 .style("top", "80px")
 
+            d3.select(".quebec-graph").style("left", String(sideCountryInit - sideCountryFactor * factor) + "px")
+
+            replaceElementScrolly(55 * factor,
+                coordLineSwedenInit1.x1 + (coordLineSwedenFinal1.x1 - coordLineSwedenInit1.x1) * factor,
+                coordLineSwedenInit1.x2 + (coordLineSwedenFinal1.x2 - coordLineSwedenInit1.x2) * factor,
+                coordLineSwedenInit2.x1 + (coordLineSwedenFinal2.x1 - coordLineSwedenInit2.x1) * factor,
+                coordLineSwedenInit2.x2 + (coordLineSwedenFinal2.x2 - coordLineSwedenInit2.x2) * factor,
+                textCoordSwedenInit.x + (textCoordSwedenFinal.x - textCoordSwedenInit.x) * factor,
+                dimensionQuebecInit.width + (dimensionQuebecFinal.width - dimensionQuebecInit.width) * factor,
+                dimensionSwedenInit.width + (dimensionSwedenFinal.width - dimensionSwedenInit.width) * factor)
+
+        } else if (posScroll >= 1200 && posScroll < 1800) {
+
+            const sideCountryInit = (windowWidth - dimensionSwedenInit.width - dimensionQuebecInit.width) / 2 - 45
+            const sideCountryFactor = (dimensionQuebecFinal.width + dimensionSwedenFinal.width - dimensionQuebecInit.width - dimensionSwedenInit.width) / 2
+            d3.select(".sweden-graph").style("right", String(sideCountryInit - sideCountryFactor) + "px")
+
+            d3.select(".quebec-graph").style("left", String(sideCountryInit - sideCountryFactor) + "px")
+
+            replaceElementScrolly(55, coordLineSwedenFinal1.x1, coordLineSwedenFinal1.x2, coordLineSwedenFinal2.x1, coordLineSwedenFinal2.x2, textCoordSwedenFinal.x, dimensionQuebecFinal.width, dimensionSwedenFinal.width)
+
+            statePicto("block")
+
+            d3.selectAll(".picto-country").style("opacity", "0%")
+
+            d3.select(".legend-picto")
+                .style("position", "fixed")
+                .style("left", "47%")
+                .style("top", "575px")
+                .style("opacity", "0%")
+
+        } else if (posScroll >= 1800 && posScroll < 2800) {
+
+            const factor = (posScroll - 1800) / 600
+            d3.select(".legend-picto").style("opacity", String(100 * factor) + "%")
+            d3.selectAll(".picto-country").style("opacity", String(100 * factor) + "%")
+
+        } else if (posScroll >= 2800) {
+            //d3.select("#map")
         } else {
 
+            statePicto("none")
+
             d3.select(".sweden-graph").style("display", "none")
+
+            d3.select(".quebec-graph").style("left", String(window.innerWidth / 2 - dimensionQuebecInit.width / 2) + "px")
 
         }
 
@@ -195,10 +251,37 @@ export function load() {
         ticking = true;
     });
 
+    function statePicto(state) {
+        d3.selectAll(".picto-country").style("display", state)
+        d3.selectAll(".legend-picto").style("display", state)
+    }
+
+    function replaceElementScrolly(swedenMapX, swedenLine1X1, swedenLine1X2, swedenLine2X1, swedenLine2X2, textSwedenX, dimensionQuebecX, dimensionSwedenX) {
+        d3.select("#quebec-title").style("width", String(dimensionQuebecX) + "px")
+        d3.select("#sweden-title").style("width", String(dimensionSwedenX) + "px")
+
+        d3.select("#sweden-map-g")
+            .selectAll("path")
+            .attr("transform", "translate(" + String(swedenMapX) + ", 0)")
+
+        var line1 = this.document.getElementById("sweden-line-map1")
+        line1.setAttribute("x1", swedenLine1X1)
+        line1.setAttribute("x2", swedenLine1X2)
+
+        var line2 = this.document.getElementById("sweden-line-map2")
+        line2.setAttribute("x1", swedenLine2X1)
+        line2.setAttribute("x2", swedenLine2X2)
+
+        this.document.getElementById("sweden-size-text").setAttribute("x", textSwedenX)
+
+        this.document.getElementById("quebec-map-svg").setAttribute("width", dimensionQuebecX)
+        this.document.getElementById("sweden-map-svg").setAttribute("width", dimensionSwedenX)
+    }
+
     function createCountrySvg(place, dimension) {
         const svg = d3.select("." + place + "-object")
             .append('svg')
-            .attr("class", "." + place + "-map-svg")
+            .attr("id", place + "-map-svg")
             .attr("width", dimension.width)
             .attr("height", dimension.height)
         return svg
@@ -222,6 +305,7 @@ export function load() {
 
         var container = svg.append("g")
 
+
         container.selectAll("use")
             .data(data)
             .enter()
@@ -241,7 +325,7 @@ export function load() {
         var g = svg.append('g')
             .attr('id', place + '-map-g')
 
-        drawText(g, textCoord, size)
+        drawText(g, textCoord, size, place + "-size-text")
 
         var projection = geoMercator()
             .center(coordMap)
@@ -261,8 +345,8 @@ export function load() {
         d3.select("#" + place + "-title")
             .style("width", String(width) + "px")
 
-        drawLine(svg, coordLine1, "")
-        drawLine(svg, coordLine2, "")
+        drawLine(svg, coordLine1, "", place + "-line-map1")
+        drawLine(svg, coordLine2, "", place + "-line-map2")
     }
 
     function drawPictogram(svg, population, coordPicto, place, coordLine1, coordLine2, size, textCoord) {
@@ -301,24 +385,25 @@ export function load() {
             .attr('y', function(d) { return y(Math.floor(d / 2)); })
             .attr('fill', "black")
 
-        drawLine(svg, coordLine1, "picto-country")
-        drawLine(svg, coordLine2, "picto-country")
+        drawLine(svg, coordLine1, "picto-country", "")
+        drawLine(svg, coordLine2, "picto-country", "")
 
         var g = svg.append('g')
             .attr("class", "picto-country")
 
 
-        drawText(g, textCoord, size)
+        drawText(g, textCoord, size, "")
     }
 
-    function drawText(g, coord, text) {
-        g.append("text")
-            .attr("x", coord.x)
+    function drawText(g, coord, text, idName) {
+        var textElement = g.append("text")
+        textElement.attr("x", coord.x)
             .attr("y", coord.y)
             .text(text)
+        if (idName != "") textElement.attr("id", idName)
     }
 
-    function drawLine(svg, coord, className) {
+    function drawLine(svg, coord, className, idName) {
         var line = svg.append('line')
             .attr("x1", coord.x1)
             .attr("y1", coord.y1)
@@ -326,5 +411,6 @@ export function load() {
             .attr("y2", coord.y2)
             .attr("style", "stroke:black")
         if (className != "") line.attr("class", className)
+        if (idName != "") line.attr("id", idName)
     }
 }
