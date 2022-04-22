9c851860dcd153922e4813060301acaed2852b52
just13564 <justin.caisse@gmail.com>
2022-04-22 17:23:08 -0400

    réalignement des lignes et des text dans la viz 1

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,19 +1,18 @@
 import { geoPath } from "d3-geo"
 import { geoMercator } from "d3-geo"
-import line from "d3-geo/src/clip/line"
 
 const coordLineQuebec1 = {
-    x1: 100,
-    x2: 130,
-    y1: 410,
-    y2: 410
+    x1: 105,
+    x2: 134,
+    y1: 10,
+    y2: 10
 }
 
 const coordLineQuebec2 = {
-    x1: 130,
-    x2: 140,
-    y1: 410,
-    y2: 350
+    x1: 134,
+    x2: 144,
+    y1: 10,
+    y2: 70
 }
 
 const coordLineSwedenInit1 = {
@@ -48,28 +47,28 @@ const coordLineSwedenPicto1 = {
     x1: 19,
     x2: 30,
     y1: 340,
-    y2: 410
+    y2: 445
 }
 
 const coordLineSwedenPicto2 = {
     x1: 30,
     x2: 80,
-    y1: 410,
-    y2: 410
+    y1: 445,
+    y2: 445
 }
 
 const coordLineQuebecPicto1 = {
-    x1: 430,
-    x2: 440,
-    y1: 20,
-    y2: 80
+    x1: 440,
+    x2: 430,
+    y1: 340,
+    y2: 445
 }
 
 const coordLineQuebecPicto2 = {
     x1: 430,
     x2: 390,
-    y1: 20,
-    y2: 20
+    y1: 445,
+    y2: 445
 }
 
 const coordPictoSweden = {
@@ -79,32 +78,32 @@ const coordPictoSweden = {
 
 const coordPictoQuebec = {
     x: 420,
-    y: 90,
+    y: 180,
 }
 
 const dimensionQuebecInit = {
     width: 395,
-    height: 420
+    height: 460
 }
 
 const dimensionQuebecFinal = {
     width: 460,
-    height: 420
+    height: 460
 }
 
 const dimensionSwedenInit = {
     width: 280,
-    height: 420
+    height: 460
 }
 
 const dimensionSwedenFinal = {
     width: 340,
-    height: 420
+    height: 460
 }
 
 const textCoordQuebec = {
     x: 0,
-    y: 415
+    y: 15.5
 }
 
 const textCoordSwedenInit = {
@@ -119,12 +118,12 @@ const textCoordSwedenFinal = {
 
 const textCoordPictoQuebec = {
     x: 230,
-    y: 22.5
+    y: 450
 }
 
 const textCoordPictoSweden = {
     x: 87.5,
-    y: 415
+    y: 450
 }
 
 const textCoordLegend = {
@@ -225,7 +224,7 @@ export function load() {
 
             transitionViz("fixed", 0)
             d3.select(".legend-picto")
-                .style("left", "47%")
+                .style("left", "49%")
                 .style("opacity", "0%")
 
 
@@ -269,7 +268,7 @@ export function load() {
         d3.select(".sweden-graph").style("position", position)
         d3.select(".sweden-graph").style("top", String(adjustment + 80) + "px")
         d3.select(".legend-picto").style("position", position)
-        d3.select(".legend-picto").style("top", String(adjustment + 575) + "px")
+        d3.select(".legend-picto").style("top", String(adjustment + 615) + "px")
     }
 
     function statePicto(state) {
